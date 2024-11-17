bc512785d93ddf37d4a0849b115f3504089c907e
iispratami <iispratami04study@gmail.com>
2024-11-17 20:36:34 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+speed(0)
+setup(800, 500)
+
+penup()
+goto(-400, 250)
+pendown()
+
+# red rectangle 
+color("red")
+begin_fill()
+forward(800)
+right(90)
+forward(250)
+right(90)
+forward(800)
+end_fill()
+
+
+hideturtle
